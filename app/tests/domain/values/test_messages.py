47c9bb8e0955004d21b31e8e9f1f351f7e3c2131
Maks-Hunzyuk <ivaeovmaks@gmail.com>
2024-12-22 19:04:13 +0300

    added chat domain events

--- app/tests/domain/values/test_messages.py
+++ app/tests/domain/values/test_messages.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pytest
 
 from domain.entities.messages import Chat, Message
-from domain.exeptions.messages import TitleTooLongExeption
+from domain.events.messages import NewMessageReceivedEvent
 from domain.values.massages import Text, Title
 
 
@@ -41,4 +41,26 @@ def test_add_chat_to_message():
     chat.add_message(message)
 
     assert message in chat.messages
+
+
+def test_new_message_event():
+    text = Text("Hello world")
+    message = Message(text=text)
+
+    title = Title("title")
+    chat = Chat(title=title)
+
+    chat.add_message(message)
+    events = chat.pull_events()
+    pulled_events = chat.pull_events()
+
+    assert not pulled_events, pulled_events
+    assert len(events) == 1
+
+    new_event = events[0]
+
+    assert isinstance(new_event, NewMessageReceivedEvent), new_event
+    assert new_event.message_oid == message.oid
+    assert new_event.chat_oid == chat.oid
+    assert new_event.message_text == message.text.as_generic_type()
 
